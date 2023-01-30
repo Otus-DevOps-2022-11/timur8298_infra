2d508ebafb9d58eed35a7b630eb556096f54baa6
timur8298 <timur.semeneev@gmail.com>
2023-01-30 17:54:14 +0000

    HW-07 Подредактировал файлы проекта

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
@@ -25,52 +17,3 @@ module "db" {
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
 }
-#resource "yandex_vpc_network" "app-network" {
-#  name = "reddit-app-network"
-#}
-
-#resource "yandex_vpc_subnet" "app-subnet" {
-#  name           = "reddit-app-subnet"
-#  zone           = "ru-central1-a"
-#  network_id     = yandex_vpc_network.app-network.id
-#  v4_cidr_blocks = ["192.168.10.0/24"]
-#}
-#resource "yandex_compute_instance" "app" {
-#  count = var.instances
-#  name  = "reddit-app-${count.index}"
-#  resources {
-#    cores  = 2
-#    memory = 4
-#  }
-#  boot_disk {
-#    initialize_params {
-#      # Указать id образа созданного в предыдущем домашем задании
-#      image_id = var.image_id
-#    }
-#  }
-
-#  network_interface {
-#    # Указан id подсети default-ru-central1-a
-#    subnet_id = yandex_vpc_subnet.app-subnet.id
-#    nat       = true
-#  }
-
-#  metadata = {
-#    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-#  }
-#  connection {
-#    type  = "ssh"
-#    host  = self.network_interface.0.nat_ip_address
-#    user  = "ubuntu"
-#    agent = false
-#    # путь до приватного ключа
-#    private_key = file(var.private_key_path)
-#  }
-#  provisioner "file" {
-#    source      = "files/puma.service"
-#    destination = "/tmp/puma.service"
-#  }
-#  provisioner "remote-exec" {
-#    script = "files/deploy.sh"
-#  }
-#}
