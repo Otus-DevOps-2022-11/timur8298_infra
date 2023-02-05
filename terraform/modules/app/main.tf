e90598c3690c1444df28feb093df887e27c7246d
timur8298 <timur.semeneev@gmail.com>
2023-02-05 17:15:17 +0000

    HW-09 Один плейбук, один сценарий

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,11 +1,11 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#    }
-#  }
-#  required_version = ">= 0.13"
-#}
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
 
@@ -39,12 +39,12 @@ resource "yandex_compute_instance" "app" {
     agent       = false
     private_key = file(var.private_key_path)
   }
-  provisioner "file" {
-    content     = templatefile("${path.module}/files/puma.service.tmpl", { db_ip = var.db_ip })
-    destination = "/tmp/puma.service"
-  }
+#  provisioner "file" {
+#    content     = templatefile("${path.module}/files/puma.service.tmpl", { db_ip = var.db_ip })
+#    destination = "/tmp/puma.service"
+#  }
 
-  provisioner "remote-exec" {
-    script = "${path.module}/files/deploy.sh"
-  }
+#  provisioner "remote-exec" {
+#    script = "${path.module}/files/deploy.sh"
+#  }
 }
